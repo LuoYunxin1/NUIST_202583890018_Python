49fab95721582f80760c6ac0ef0db358dd5dcba4
Luoyunxin <202583890018@nuist.edu.cn>
2026-03-19 16:02:03 +0800

    Version 2

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,8 +1,12 @@
 # Add Two Number in Pythen 
 #Author :Luoyunxin
-# Using the + Operator
-a = 15
-b = 12
-#Adding two numbers
-res = a + b
+# Using user input 
+
+#taking user input 
+a =input("First number:")
+b = input("Sencond number:")
+
+#convering input to float and adding
+res = float(a) + float(b)
 print (res)
+
